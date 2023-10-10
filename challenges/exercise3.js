043ddbe03e538ca7048fe5e30e9cb14bbb47727c
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-10-10 17:14:07 +0100

    feat: Returns an array of names beginning with the specified character

--- challenges/exercise3.js
+++ challenges/exercise3.js
@@ -6,7 +6,7 @@ export function findSmallNums(nums) {
 export function findNamesBeginningWith(names, char) {
 	if (!names) throw new Error('names is required');
 	if (!char) throw new Error('char is required');
-	// Your code here
+	return names.filter(name => name.startsWith(char));
 }
 
 export function findVerbs(words) {
