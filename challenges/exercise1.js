038eb35ff5d2ae5bb411a87f12abd98b97cfbdaf
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-10-10 12:02:33 +0100

    feat: Returns the filling of a sandwich

--- challenges/exercise1.js
+++ challenges/exercise1.js
@@ -1,6 +1,6 @@
 export function getFillings(sandwich) {
 	if (sandwich === undefined) throw new Error('ingredients is required');
-	// Your code here!
+	return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
