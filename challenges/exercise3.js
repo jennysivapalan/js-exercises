37f51c47dade52074fe493d3da78849b0a96bd19
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-10-10 17:20:04 +0100

    feat: Returns an array of words that are considered verbs

--- challenges/exercise3.js
+++ challenges/exercise3.js
@@ -11,7 +11,7 @@ export function findNamesBeginningWith(names, char) {
 
 export function findVerbs(words) {
 	if (!words) throw new Error('words is required');
-	// Your code here
+	return words.filter(word => word.startsWith('to '))
 }
 
 export function getIntegers(nums) {
