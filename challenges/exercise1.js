e886fb9dab77319f4123c10f8a8716cd87aa8c58
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-10-11 16:58:19 +0100

    style: Adds prettier format to previous exercises and test files

--- challenges/exercise1.js
+++ challenges/exercise1.js
@@ -1,28 +1,29 @@
 export function getFillings(sandwich) {
-	if (sandwich === undefined) throw new Error('ingredients is required');
-	return sandwich.fillings;
+  if (sandwich === undefined) throw new Error("ingredients is required");
+  return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
-	if (person === undefined) throw new Error('person is required');
-	return person.city === "Manchester";
+  if (person === undefined) throw new Error("person is required");
+  return person.city === "Manchester";
 }
 
 export function getBusNumbers(people) {
-	if (people === undefined) throw new Error('people is required');
-	const numberOfBuses = people / 40
-	return Math.ceil(numberOfBuses);
+  if (people === undefined) throw new Error("people is required");
+  const numberOfBuses = people / 40;
+  return Math.ceil(numberOfBuses);
 }
 
 export function countSheep(arr) {
-	if (arr === undefined) throw new Error('arr is required');
-	const numberOfSheep = arr.filter(animal => animal === "sheep")
-	return numberOfSheep.length;
+  if (arr === undefined) throw new Error("arr is required");
+  const numberOfSheep = arr.filter((animal) => animal === "sheep");
+  return numberOfSheep.length;
 }
 
 export function hasMPostCode(person) {
-	if (person === undefined) throw new Error('person is required');
-	const postcode = person.address.postCode;
-	if(postcode.startsWith("M") && person.address.city === "Manchester") return true;
-	else return false;
+  if (person === undefined) throw new Error("person is required");
+  const postcode = person.address.postCode;
+  if (postcode.startsWith("M") && person.address.city === "Manchester")
+    return true;
+  else return false;
 }
--- challenges/exercise2.js
+++ challenges/exercise2.js
@@ -1,35 +1,40 @@
 export function getSquares(nums) {
-	if (nums === undefined) throw new Error('nums is required');
-	return nums.map(number => Math.pow(number, 2));
+  if (nums === undefined) throw new Error("nums is required");
+  return nums.map((number) => Math.pow(number, 2));
 }
 
 export function camelCaseWords(words) {
-	if (words === undefined) throw new Error('words is required');
-	const wordsAfterFirstWord = words.slice(1)
-	const capitaliseWords = wordsAfterFirstWord.map(word => word[0].toUpperCase() + word.substring(1))
-	return words[0] + capitaliseWords.join('')
+  if (words === undefined) throw new Error("words is required");
+  const wordsAfterFirstWord = words.slice(1);
+  const capitaliseWords = wordsAfterFirstWord.map(
+    (word) => word[0].toUpperCase() + word.substring(1)
+  );
+  return words[0] + capitaliseWords.join("");
 }
 
 export function getTotalSubjects(people) {
-	if (people === undefined) throw new Error('people is required');
-	return people.reduce((accumalator, person) => accumalator + person.subjects.length, 0)
+  if (people === undefined) throw new Error("people is required");
+  return people.reduce(
+    (accumalator, person) => accumalator + person.subjects.length,
+    0
+  );
 }
 
 export function checkIngredients(menu, ingredient) {
-	if (menu === undefined) throw new Error('menu is required');
-	if (!ingredient) throw new Error('ingredient is required');
-	for( let i = 0; i < menu.length; i++) {
-		if(menu[i].ingredients.includes(ingredient)) return true;
-	}
-	return false;
+  if (menu === undefined) throw new Error("menu is required");
+  if (!ingredient) throw new Error("ingredient is required");
+  for (let i = 0; i < menu.length; i++) {
+    if (menu[i].ingredients.includes(ingredient)) return true;
+  }
+  return false;
 }
 
 export function duplicateNumbers(arr1, arr2) {
-	if (arr1 === undefined) throw new Error('arr1 is required');
-	if (arr2 === undefined) throw new Error('arr2 is required');
-	const duplicateNumbers = [];
-	arr1.forEach(element => {
-		if(arr2.includes(element)) duplicateNumbers.push(element);
-	});
-	return [...new Set(duplicateNumbers.sort())];
+  if (arr1 === undefined) throw new Error("arr1 is required");
+  if (arr2 === undefined) throw new Error("arr2 is required");
+  const duplicateNumbers = [];
+  arr1.forEach((element) => {
+    if (arr2.includes(element)) duplicateNumbers.push(element);
+  });
+  return [...new Set(duplicateNumbers.sort())];
 }
